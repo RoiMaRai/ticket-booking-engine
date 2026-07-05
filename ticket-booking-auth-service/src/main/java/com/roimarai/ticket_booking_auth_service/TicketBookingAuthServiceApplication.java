1e53faa2828e5acf9d58fbfadd3cf43a11b17a26
RoiMaRai <sathaporn.permpoca@gmail.com>
2026-07-05 15:40:09 +0700

    Spring Boot skeleton

---
+++ ticket-booking-auth-service/src/main/java/com/roimarai/ticket_booking_auth_service/TicketBookingAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roimarai.ticket_booking_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingAuthServiceApplication.class, args);
+	}
+
+}
---
+++ ticket-booking-auth-service/src/test/java/com/roimarai/ticket_booking_auth_service/TicketBookingAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roimarai.ticket_booking_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
