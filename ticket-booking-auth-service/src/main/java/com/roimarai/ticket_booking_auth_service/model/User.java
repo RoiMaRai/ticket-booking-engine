0f55e185cb722fcad9083124955b323b466718a2
RoiMaRai <sathaporn.permpoca@gmail.com>
2026-07-10 16:56:04 +0700

    Made /register and /login accessible to everyone; Finished /register

--- ticket-booking-auth-service/src/main/java/com/roimarai/ticket_booking_auth_service/model/User.java
+++ ticket-booking-auth-service/src/main/java/com/roimarai/ticket_booking_auth_service/model/User.java
@@ -1,11 +1,9 @@
 package com.roimarai.ticket_booking_auth_service.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
