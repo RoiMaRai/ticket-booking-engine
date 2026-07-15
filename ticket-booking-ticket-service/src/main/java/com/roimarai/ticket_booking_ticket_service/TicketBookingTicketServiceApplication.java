4798a321062511b20b2e913942e178912dbce705
RoiMaRai <sathaporn.permpoca@gmail.com>
2026-07-15 23:56:11 +0700

    chore: add ticket_booking_ticket_service skeleton

---
+++ ticket-booking-ticket-service/src/main/java/com/roimarai/ticket_booking_ticket_service/TicketBookingTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roimarai.ticket_booking_ticket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingTicketServiceApplication.class, args);
+	}
+
+}
---
+++ ticket-booking-ticket-service/src/test/java/com/roimarai/ticket_booking_ticket_service/TicketBookingTicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roimarai.ticket_booking_ticket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingTicketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
