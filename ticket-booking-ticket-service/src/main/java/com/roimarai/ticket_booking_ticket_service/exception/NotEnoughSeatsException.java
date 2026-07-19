a902d6be206fa12c2e2df2dac3d7938f6d5a25f4
RoiMaRai <sathaporn.permpoca@gmail.com>
2026-07-19 17:25:29 +0700

    feat(ticket-service): add NotEnoughSeatsException for overbooking

--- ticket-booking-ticket-service/src/main/java/com/roimarai/ticket_booking_ticket_service/exception/NotEnoughSeatsException.java
+++ ticket-booking-ticket-service/src/main/java/com/roimarai/ticket_booking_ticket_service/exception/NotEnoughSeatsException.java
@@ -3,9 +3,9 @@ package com.roimarai.ticket_booking_ticket_service.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class NotEnoughSeatsException extends RuntimeException {
+    public NotEnoughSeatsException(String message) {
         super(message);
     }
 }
