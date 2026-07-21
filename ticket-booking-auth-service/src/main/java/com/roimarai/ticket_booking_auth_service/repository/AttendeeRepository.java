5ca187fb9caa86fcf2cb09e3e36a09389387fb75
RoiMaRai <sathaporn.permpoca@gmail.com>
2026-07-21 22:27:05 +0700

    chore(auth-service): remove throwaway controllers used to practice endpoints (AttendeeController,GreetController,HelloController,ServiceInfoController,StatusController,VersionController)

--- ticket-booking-auth-service/src/main/java/com/roimarai/ticket_booking_auth_service/repository/AttendeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.roimarai.ticket_booking_auth_service.repository;
-
-import com.roimarai.ticket_booking_auth_service.model.Attendee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendeeRepository extends JpaRepository<Attendee, Long> {
-}
